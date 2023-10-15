c758cbdbb7b305206d3d00951ee5e98210ecee73
Anton Yaskov <yaskovanton333@gmail.com>
2023-10-15 20:02:03 +0500

    Added italic tag

---
+++ API/API/Modules/MarkdownModule/Models/Tags/ItalicTag.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Modules.MarkdownModule.Models.Tags;
+
+public record struct ItalicTag : IMarkdownTag
+{
+    public string HtmlTag => htmlTag;
+    public bool HasHtmlPair => true;
+    public string OpenTag => tag;
+    public string CloseTag => tag;
+    
+    private const string tag = "_";
+    private const string htmlTag = "em";
+}
