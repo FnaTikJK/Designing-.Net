50d76551982e9017e32575f3f71d026b85cf4d3e
Anton Yaskov <yaskovanton333@gmail.com>
2023-10-15 20:01:26 +0500

    Added base entities

---
+++ API/API/Modules/MarkdownModule/Models/Tags/IMarkdownTag.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Modules.MarkdownModule.Models.Tags;
+
+public interface IMarkdownTag
+{
+    public string HtmlTag { get; }
+    public bool HasHtmlPair { get; }
+    public string OpenTag { get; }
+    public string CloseTag { get; }
+    public char Shield => '\\';
+}
---
+++ API/API/Modules/MarkdownModule/Models/TokenBorder.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Modules.MarkdownModule.Models;
+
+public class TokenBorder
+{
+    public int? Index { get; set; }
+    public (char? Before, char? After) Neighbors { get; set; }
+}
